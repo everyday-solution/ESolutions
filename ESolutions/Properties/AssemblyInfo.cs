4adcc67c0ece6ea908847839ab50f9a5d5267907
Tobias Mundt <ich@tobiasmundt.de>
2017-09-23 00:50:04 +0200

    Updated nuspec

--- ESolutions/Properties/AssemblyInfo.cs
+++ ESolutions/Properties/AssemblyInfo.cs
@@ -6,13 +6,17 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle ("ESolutions")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyDescription ("Contains basic tools for Rjindael encryption, password hashing and type conversion as well as helpful extensions for basic typs like Images, String and Lists.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 [assembly: AssemblyCompany ("Tobias Mundt - everyday Solutions")]
 [assembly: AssemblyProduct ("ESolutions")]
 [assembly: AssemblyCopyright ("Copyright © Tobias Mundt - everyday Solutions 2014")]
 [assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyCulture ("en-US")]
 
 // Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
 // für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
