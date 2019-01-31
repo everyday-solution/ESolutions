5ec25858067559aa1c82a7e8025c1deea32d2475
Tobias Mundt <ich@tobiasmundt.de>
2019-01-31 18:54:52 +0100

    Added menu for console apps

---
+++ ESolutions.Core/Console/ConsoleMenu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ESolutions.Core.Console
+{
+	public static class ConsoleMenu
+	{
+		//Fields
+		#region exitChar
+		private const Char exitChar = 'x';
+		#endregion
+
+		//Methods
+		#region PerformMenuLoop
+		public static void Show(List<MenuItem> menuItems)
+		{
+			var keyChar = exitChar;
+			do
+			{
+				System.Console.WriteLine("=================");
+				foreach (var runner in menuItems)
+				{
+					System.Console.WriteLine($"{runner.Key}) {runner.Text}");
+				}
+				System.Console.WriteLine($"{exitChar}) Exit");
+				System.Console.Write("Your selection: ");
+				keyChar = System.Console.ReadKey().KeyChar;
+				System.Console.WriteLine();
+
+				var menuItem = menuItems.FirstOrDefault(runner => runner.Key == keyChar);
+				if (menuItem != null)
+				{
+					menuItem.Action();
+					System.Console.WriteLine();
+				}
+				else
+				{
+					System.Console.WriteLine("Unknown key");
+				}
+				System.Console.WriteLine();
+			}
+			while (keyChar != exitChar);
+
+			System.Console.WriteLine("Bye Bye!");
+			Thread.Sleep(1000);
+		}
+		#endregion
+	}
+}
---
+++ ESolutions.Core/Console/MenuItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESolutions.Core.Console
+{
+	public class MenuItem
+	{
+		//Properties
+		#region Key
+		public Char Key
+		{
+			get;
+			private set;
+		}
+		#endregion
+
+		#region Text
+		public String Text
+		{
+			get;
+			private set;
+		}
+		#endregion
+
+		#region Action
+		public Action Action
+		{
+			get;
+			private set;
+		}
+		#endregion
+
+		//Constructor
+		#region MenuItem
+		public MenuItem(Char key, String text, Action action)
+		{
+			this.Key = key;
+			this.Text = text;
+			this.Action = action;
+		}
+		#endregion
+	}
+}
